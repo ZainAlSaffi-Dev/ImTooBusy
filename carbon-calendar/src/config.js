b7f828545739929d89e4aed217a466f112b98eb4
ZainAlSaffi-Dev <hypersonic3692@gmail.com>
2026-01-27 23:28:02 +1000

    Changing to exporting const instead of default

--- carbon-calendar/src/config.js
+++ carbon-calendar/src/config.js
@@ -1,5 +1,4 @@
-const config = {
-    API_BASE_URL: import.meta.env.VITE_API_URL || "http://127.0.0.1:8000",
-  };
-  
-export default config;
+// src/config.js
+
+// Automatically switches between Localhost and Real Cloud URL
+export const API_BASE_URL = import.meta.env.VITE_API_URL || "http://127.0.0.1:8000";
